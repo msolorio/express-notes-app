38c362fb73f752e981b981003e7aba72946f58bf
msolorio <msolorio789@gmail.com>
2020-06-18 17:23:24 -0700

    add route handlers for get to /api/notes and delete to /api/delete

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -6,17 +6,48 @@ const router = express.Router();
 const readFilePromisify = util.promisify(fs.readFile);
 const writeFilePromisify = util.promisify(fs.writeFile);
 
+function readFromDb() {
+  return readFilePromisify(path.join(__dirname, "../db/db.json"))
+    .then((jsonData) => JSON.parse(jsonData));
+};
+
+function writeToDb(parsedData) {
+  const stringifiedData = JSON.stringify(parsedData);
+  return writeFilePromisify(path.join(__dirname, "../db/db.json"), stringifiedData);
+};
+
 router.post("/notes", (req, res) => {
   const newNote = req.body;
 
-  readFilePromisify(path.join(__dirname, "../db/db.json"))
-    .then((jsonData) => {
-      const parsedData = JSON.parse(jsonData);
+  readFromDb()
+    .then((parsedData) => {
+      const noteId = parsedData.length;
+      newNote.id = noteId;
       parsedData.push(newNote);
-      const stringifiedData = JSON.stringify(parsedData);
-
-      return writeFilePromisify(path.join(__dirname, "../db/db.json"), stringifiedData);
+      
+      return parsedData;
     })
+    .then((updatedData) => {
+      writeToDb(updatedData);
+    })
+    .then(() => res.end())
+    .catch((err) => {
+      if (err) console.error(err);
+    });
+});
+
+router.get('/notes', (req, res) => {
+  readFromDb()
+    .then((parsedData) => res.json(parsedData))
+    .catch((err) => {
+      if (err) console.error(err);
+    })
+});
+
+router.delete('/notes/:id', (req, res) => {
+  readFromDb()
+    .then((parsedData) => parsedData.filter((note) => note.id !== parseInt(req.params.id, 10)))
+    .then((filteredData) => writeToDb(filteredData))
     .then(() => res.end())
     .catch((err) => {
       if (err) console.error(err);
